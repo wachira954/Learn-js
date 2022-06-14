94f2ce97bbe67a61c4cbbea17d6e134191fba1a1
wachira <wachiraa954@gmail.com>
2022-06-14 23:49:09 +0300

    add variables file

---
+++ variables.js
@@ -0,0 +1,35 @@
+//boolean variables
+let isTrue =true
+let isFalse =false
+
+//string
+
+let firstName ="true"
+let sTrue = true
+console.log(firstName == sTrue)
+
+//console.log(typeof(isTrue))
+//console.log(typeof(sTrue))
+
+//Numbers
+var age =3;
+
+//undefined
+
+const isUndefined = undefined;
+
+//console.log(isUndefined)
+
+const isNull=null;
+//console.log(isNull)
+
+let kienyeji = 'queen'
+//console.log(kienyeji)
+
+
+
+
+
+
+
+
